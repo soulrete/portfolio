0cafb8224f96875dfc3d99b2b1ef72bdc5d4b784
soulrete <42328378+soulrete@users.noreply.github.com>
2024-01-16 16:33:11 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ document.addEventListener('scroll', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
-    console.log(1 - window.scrollY / homeHeight);
     /* 
         계산식 도출
         scrollY   homeHeight   opacity
@@ -25,4 +24,14 @@ document.addEventListener('scroll', () => {
         이렇게 써보고 계산식 도출하기.
     */
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
 });
