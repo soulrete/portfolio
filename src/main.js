55c5cad5216d33a4f621ea8fd0f55163bfa5a98c
soulrete <42328378+soulrete@users.noreply.github.com>
2024-01-16 19:19:27 +0900

    JS-Files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
@@ -34,4 +36,16 @@ document.addEventListener('scroll', () => {
     } else {
         arrowUp.style.opacity = 0;
     }
+});
+
+// Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
 });
